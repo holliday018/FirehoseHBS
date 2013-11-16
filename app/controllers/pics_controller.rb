ec693068c1d4bc40ef839af32ccb233f124f0658
Joseph Holliday <JLH@josephs-macbook.hbs.edu>
2013-11-16 17:24:34 -0500

    Finished form to populate database.

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,9 +1,26 @@
 class PicsController < ApplicationController
 
-def index
+	def index
 
-	@pics = Pic.all
+		@pics = Pic.all
 
-end
+	end
 
+	def new
+
+		@pic = Pic.new
+
+	end
+
+	def create
+		@pic = Pic.create( pic_params )
+		redirect_to pics_path
+	end
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:lesson, :emotion, :learned)
+
+	end
 end
